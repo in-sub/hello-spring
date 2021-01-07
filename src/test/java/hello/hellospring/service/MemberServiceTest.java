0556f739d5376c841a172d7c341d3044459479a7
in-sub <bingsu517@gmail.com>
2021-01-08 07:39:50 +0900

    initial commit

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -54,7 +54,6 @@ class MemberServiceTest {
         memberService.join(member1);
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
 
-        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
         /*try {
             memberService.join(member2);
             fail();
@@ -63,6 +62,7 @@ class MemberServiceTest {
         }*/
 
         //then
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
     }
     @Test
     void findMember() {
