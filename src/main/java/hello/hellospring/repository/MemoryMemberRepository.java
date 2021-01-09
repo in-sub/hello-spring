ab9123a8f713181f944fb7eca52ef8a8df7d42ef
in-sub <bingsu517@gmail.com>
2021-01-09 22:01:54 +0900

    스프링빈 - 컴포넌트 스캔과 자동 의존관계 설정

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -2,10 +2,11 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Autowired
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
