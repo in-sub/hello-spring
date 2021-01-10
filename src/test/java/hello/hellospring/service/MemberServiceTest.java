cdf255f676ba1c4c7577dc95853895a8c4dd3d34
in-sub <bingsu517@gmail.com>
2021-01-10 14:38:01 +0900

    스프링 DB 접근 기술 - JPA

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,6 +31,7 @@ class MemberServiceTest {
     }
 
     @Test
+    @Commit
     void 회원가입() {
         //given
         Member member = new Member();
